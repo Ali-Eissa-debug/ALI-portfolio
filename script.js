1e63209bbf8e1a57950ed45ff58332be50157d60
Ali-Eissa-debug <eissaali108@gmail.com>
2026-03-13 18:40:09 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// ========================= script.js =========================
+
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.getElementById("navLinks");
+
+menuBtn.onclick = () =>{
+navLinks.classList.toggle("active");
+};
